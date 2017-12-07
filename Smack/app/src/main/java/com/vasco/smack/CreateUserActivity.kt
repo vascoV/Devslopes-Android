6a128443ae05d6264596d1a756ed938387378184
vascoV <vassco.vasilev@gmail.com>
2017-12-07 22:44:06 +0200

    UPDATES

---
+++ Smack/app/src/main/java/com/vasco/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.vasco.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateColorClick(view: View) {
+
+    }
+
+    fun createUserClick(view: View) {
+
+    }
+}
--- Smack/app/src/main/java/com/vasco/smack/LoginActivity.kt
+++ Smack/app/src/main/java/com/vasco/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.vasco.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserClick(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
