ed2386de6ce6a18919e2f32115530d5b4fedf73c
vascoV <vassco.vasilev@gmail.com>
2017-12-15 22:13:43 +0200

    UPDATES

--- Smack/app/src/main/java/com/vasco/smack/CreateUserActivity.kt
+++ Smack/app/src/main/java/com/vasco/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.vasco.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,32 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2) // the nu,ber here is the upper bound and is not included, we are generating nums between 0 and 1
+        val avatar = random.nextInt(28)
 
+        if (color == 0) userAvatar = "light$avatar"
+        else userAvatar = "dark$avatar"
+
+        val resourceID = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceID)
     }
 
     fun generateColorClick(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        // Creates value equivalen to 255 but the the scale between 0 - 1
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println(avatarColor)
     }
 
     fun createUserClick(view: View) {
